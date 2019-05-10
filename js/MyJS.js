2d44bba0b3ae11822beccdd3e6e743d6bed598cb
itiandong <j@itiandong.com>
2019-05-10 23:44:39 +0800

    Add onClick function for tag. :kissing_heart:

--- js/MyJS.js
+++ js/MyJS.js
@@ -1,5 +1,7 @@
-let moto = document.getElementById('moto-words');
+"use strict"
 
+// 实现座右铭点击切换。
+let moto = document.getElementById('moto-words');
 let index = 0;
 const motos = [
     'Later equals never.',
@@ -11,4 +13,20 @@ const motos = [
 moto.onclick = function() {
     index = (index + 1) % 5;
     moto.innerHTML = motos[index];
+}
+
+// 实现 tag 点击展开
+let tags = document.getElementsByClassName("tag");
+for (let i = 0; i < tags.length; i++) {
+    tags[i].onclick = function(){
+        // toggle 改变 "active" 属性：
+        // 如果存在就删掉，不存在就添加并且设置为true
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
 }
